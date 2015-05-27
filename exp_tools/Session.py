c04a0104cbd54ce94e937d694aede6d2e2c85a7b
Tomas Knapen <tknapen@gmail.com>
2015-05-27 11:37:40 +0200

    some non descript change to session.py in the exp_tools folder

--- exp_tools/Session.py
+++ exp_tools/Session.py
@@ -220,13 +220,14 @@ class EyelinkSession(Session):
 			# create actual tracker
 			try:
 				# self.tracker = EyeLink()
-				self.tracker = eyetracker.EyeTracker(self.display, trackertype='eyelink', resolution=self.display.dispsize, data_file=self.eyelink_temp_file, bgc=self.display.bgc)
+				self.tracker = eyetracker.EyeTracker(self.display, trackertype='dummy', resolution=self.display.dispsize, data_file=self.eyelink_temp_file, bgc=self.display.bgc)
 				self.tracker_on = True
-			except RuntimeError:
+			except:
 				print '\ncould not connect to tracker'
 				self.tracker = None
 				self.tracker_on = False
 				self.eye_measured, self.sample_rate, self.CR_mode, self.file_sample_filter, self.link_sample_filter = 'N', sample_rate, 1, 1, 1
+
 				return
 		else:
 			# not even create dummy tracker

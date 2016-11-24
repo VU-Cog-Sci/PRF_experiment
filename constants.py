8ca0b05f074ad82b9b4ea2d423a729a7689ad610
daanvanes <daan.van.es@gmail.com>
2016-11-24 13:18:13 +0100

    (1) removed button signs, and (2) background_color to BCG, (3) removed test stim color params

--- constants.py
+++ constants.py
@@ -15,11 +15,8 @@ standard_parameters = {
     'test_stim_y_offset':                2,# 2,    #if 0, takes mid between fp and y boundary
     'test_stim_height':                 2,#3,        # if 0, takes 1/3 of available y space
     'test_stim_width':                  0.4,        # 0.125 * 0.
-    'sp_target_size':                   4,        #
+    'sp_target_size':                   1,        #
     'sp_path_temporal_frequency':       0.5,#25,#1.0,        #
-    'test_stim_1_color':                (1,1,1),#(1, -1, -1),        # 
-    'test_stim_2_color':                (1,1,1),#(-1, 1, -1),        # 
-
     'test_stim_positions':              [-3,-1,0,1,3],        # 
 
     ## mapper variables:
@@ -33,25 +30,22 @@ standard_parameters = {
     'pupil_tracking_mode':              0,# 0: centroid, 1 = ellipse
 }
 
-response_button_signs = {
-'e':-1,  # left answer 
-'b':1,   # right answer   
-'y':0}   # confirm color match
-# }      
     
-background_color = (-1, -1, -1)#-0.75,-0.75,-0.75)
-full_screen = False
-# screen settings:
-DISPSIZE = (1280,720)#,1080)#(1024,768)#(1920,1080)
-SCREENSIZE = (79.7,49.1)#(39.0,29.0)#(33.8,27.1) # physical screen size in centimeters
-SCREENDIST = 225#60.0#65.0#57.0 # centimeters; distance between screen and participant's eyes
-FGC = (255,255,255)
-BGC = (0,0,0)
-# FGC = (0,0,0)
-# BGC = (255,255,255)
+#############################################
+# screen settings 
+#############################################
 
-#(1280,1024)#(1024,768)#(1680,1050)#(1280,1024) # canvas size
-# in case of 7T scanner:
-# SCREENSIZE = 
-# SCREENDIST = 225
-# K2D-38:
+full_screen = True
+FGC = (255,0,0)
+BGC = (0,0,0) # this is converted to -1<->1 in SPSession
+
+
+# K2D-38 (all in cm):
+DISPSIZE = (1024,768)
+SCREENSIZE = (39.0,29.0)
+SCREENDIST = 60.0
+#7T scan room (all in cm):
+#DISPSIZE = (1280,720)#(1280,720)#,1080)#(1024,768)#(1920,1080)
+#SCREENSIZE = (69.84,39.29)#(79.7,49.1)#(39.0,29.0)#(33.8,27.1) # physical screen size in centimeters
+#SCREENDIST = 225#60.0#65.0#57.0 # centimeters; distance between screen and participant's eyes
+

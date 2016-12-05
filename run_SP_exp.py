62497847a0d26e79b5662d67affc70fad9ed1176
daanvanes <daan.van.es@gmail.com>
2016-12-05 13:12:03 +0100

    added TR angulation warning

--- run_SP_exp.py
+++ run_SP_exp.py
@@ -9,17 +9,21 @@ try:
 except:
     print 'APPNOPE NOT ACTIVE!'
 
+from constants import *
+
 def main():
     initials = raw_input('Your initials: ')
     run_nr = int(raw_input('Run number: '))
     fix_sp = raw_input('Set SP amplitude to 0 (y/n): ')
     scanner = raw_input('Are you in the scanner (y/n)?: ')
     track_eyes = raw_input('Are you recording gaze (y/n)?: ')
+    check_TRs = raw_input('You sure TR is set %.3f after angulation (y/n)?'%standard_parameters['TR'])
+    if check_TRs == 'n':
+        exit()
     if track_eyes == 'y':
         tracker_on = True
     elif track_eyes == 'n':
         tracker_on = False
-
 
     ts = SPSession( initials, run_nr, scanner, tracker_on,fix_sp )
     ts.run()

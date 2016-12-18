7b150bef4b4740484a1c1a58cb31496bd8ee1360
daanvanes <daan.van.es@gmail.com>
2016-12-18 18:49:44 +0100

    added task arg

--- run_PRF_exp.py
+++ run_PRF_exp.py
@@ -4,21 +4,24 @@ sys.path.append( 'exp_tools' )
 
 from PRFSession import *
 from plot_staircases import *
-import appnope
+try:
+    import appnope
+    appnope.nope()
+except:
+    print 'APPNOPE NOT ACTIVE!'
 
 def main():
 	initials = raw_input('Your initials: ')
 	run_nr = int(raw_input('Run number: '))
 	scanner = raw_input('Are you in the scanner (y/n)?: ')
 	track_eyes = raw_input('Are you recording gaze (y/n)?: ')
+	task_type = raw_input('Attention task (fix/bar): ')
 	if track_eyes == 'y':
 		tracker_on = True
 	elif track_eyes == 'n':
 		tracker_on = False
 
-	appnope.nope()
-
-	ts = PRFSession( initials, run_nr, scanner, tracker_on )
+	ts = PRFSession( initials, run_nr, scanner, tracker_on,task_type )
 	ts.run()
 
 	plot_staircases(initials, run_nr)
